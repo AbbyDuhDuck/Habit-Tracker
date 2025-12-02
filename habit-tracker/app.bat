717b566328a4deb7a22512549ace8d251104e2d7
Abby Duck <abbyduck96@gmail.com>
2025-12-02 07:51:13 -0500

    Add simple batch file to run app for testing

---
+++ habit-tracker/app.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+
+@REM This file is for ease fo testing the application and quickly runs it from the file explorer.
+
+ECHO "Starting Habit Tracker Application"
+ECHO.
+
+python app
+
+ECHO.
+PAUSE
